b56414a684d21afe72e170942dbc03286288e1f9
wandri <wandrille.guesdon@akur8-tech.com>
2020-12-22 18:56:45 +0100

    add new component

---
+++ src/app/formula-input/formula-input.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formula-input',
+  templateUrl: './formula-input.component.html',
+  styleUrls: ['./formula-input.component.scss']
+})
+export class FormulaInputComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
