cb8be02a77b5ebe71386c572ebb426e4e4321dae
wandri <wandrille.guesdon@akur8-tech.com>
2020-12-22 22:38:42 +0100

    add new component

--- src/app/formula-input/formula-input.component.spec.ts
+++ src/app/formula-input/formula-input.component.spec.ts
@@ -8,18 +8,83 @@ describe('FormulaInputComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ FormulaInputComponent ]
+      declarations: [FormulaInputComponent]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
-  beforeEach(() => {
+  beforeEach(async(() => {
     fixture = TestBed.createComponent(FormulaInputComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+
+    component.formulas = {
+      ids: ['SUM', 'SOM', 'PROD'],
+      item: {
+        ['SUM']: { name: 'SUM' },
+        ['PROD']: { name: 'PROD' },
+        ['SOM']: { name: 'SOM' },
+      }
+    };
+  }));
 
   it('should create', () => {
-    expect(component).toBeTruthy();
+    const input = fixture.debugElement.nativeElement.querySelector('.cell-input');
+    expect(input).toBeTruthy();
+  });
+
+  it('should display nothing when the user write something', () => {
+    const input = fixture.debugElement.nativeElement.querySelector('.cell-input');
+    input.dispatchEvent(new Event('input'));
+    const otherContent = fixture.debugElement.nativeElement.querySelector('.suggestions');
+    expect(otherContent).toBeFalsy();
+  });
+
+  it('should display 2 propositions if the first letter match', () => {
+    spyOn(component, 'getCaretIndex').and.returnValue(1);
+    const input = fixture.debugElement.nativeElement.querySelector('.cell-input');
+    input.innerHTML = 'S';
+    fixture.detectChanges();
+    input.dispatchEvent(new InputEvent('input'));
+    fixture.detectChanges();
+    const otherContent = fixture.debugElement.nativeElement.querySelector('.suggestions');
+    expect(otherContent).toBeTruthy();
+
+    const suggestions: NodeList = otherContent.querySelectorAll('.suggestion');
+    expect(suggestions.length).toEqual(2);
+  });
+
+  it('the matching is case sensitive', () => {
+    const input = fixture.debugElement.nativeElement.querySelector('.cell-input');
+    input.innerHTML = 's';
+    input.dispatchEvent(new InputEvent('input'));
+    fixture.detectChanges();
+    const otherContent = fixture.debugElement.nativeElement.querySelector('.suggestions');
+    expect(otherContent).toBeFalsy();
+  });
+
+  it('should filter the suggestion', () => {
+    spyOn(component, 'getCaretIndex').and.returnValue(2);
+    const input = fixture.debugElement.nativeElement.querySelector('.cell-input');
+    input.innerHTML = 'SU';
+    input.dispatchEvent(new InputEvent('input', { data: 'U' }));
+    fixture.detectChanges();
+    const otherContent = fixture.debugElement.nativeElement.querySelector('.suggestions');
+    expect(otherContent).toBeTruthy();
+
+    const suggestions: NodeList = otherContent.querySelectorAll('.suggestion');
+    expect(suggestions.length).toEqual(1);
+  });
+  it('should suggest a SUM after a SUM', () => {
+    spyOn(component, 'getCaretIndex').and.returnValue(6);
+    const input = fixture.debugElement.nativeElement.querySelector('.cell-input');
+    input.innerHTML = 'SUM(SO';
+    input.dispatchEvent(new InputEvent('input'));
+    fixture.detectChanges();
+    const otherContent = fixture.debugElement.nativeElement.querySelector('.suggestions');
+    expect(otherContent).toBeTruthy();
+
+    const suggestions: NodeList = otherContent.querySelectorAll('.suggestion');
+    expect(suggestions.length).toEqual(1);
   });
 });
--- src/app/formula-input/formula-input.component.ts
+++ src/app/formula-input/formula-input.component.ts
@@ -1,15 +1,83 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-formula-input',
   templateUrl: './formula-input.component.html',
-  styleUrls: ['./formula-input.component.scss']
+  styleUrls: ['./formula-input.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+
 })
 export class FormulaInputComponent implements OnInit {
+  suggestions: any[] = [];
+
+  formulas = {
+    ids: ['SUM', 'SOM', 'PROD'],
+    item: {
+      ['SUM']: { name: 'SUM' },
+      ['PROD']: { name: 'PROD' },
+      ['SOM']: { name: 'SOM' },
+    }
+  };
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onNameChange(event: any) {
+    const cellInput = (event.target as HTMLInputElement);
+    const propositions = [];
+
+    const caretIndex = this.getCaretIndex(cellInput);
+    const formattedContents = cellInput.innerHTML.slice(0, caretIndex).split(/[(\/+-]/);
+    const formattedContent = formattedContents[formattedContents.length - 1];
+    if (!!formattedContent) {
+      for (let name of this.formulas.ids) {
+        if (name.startsWith(formattedContent)) {
+          propositions.push(this.formulas.item[name]);
+        }
+      }
+    }
+    this.suggestions = propositions;
+    //console.log(this.getCaretCoordinates());
+  }
+
+  getCaretCoordinates() {
+    let x = 0,
+      y = 0;
+    const selection = window.getSelection();
+    // Check if there is a selection (i.e. cursor in place)
+    if (selection.rangeCount !== 0) {
+      // Clone the range
+      const range = selection.getRangeAt(0).cloneRange();
+      // Collapse the range to the start, so there are not multiple chars selected
+      range.collapse(true);
+      // getCientRects returns all the positioning information we need
+      const rect = range.getClientRects()[0];
+      if (rect) {
+        x = rect.left; // since the caret is only 1px wide, left == right
+        y = rect.top; // top edge of the caret
+      }
+    }
+    return { x, y };
+  }
+
+  getCaretIndex(element) {
+    let position = 0;
+    const selection = window.getSelection();
+    // Check if there is a selection (i.e. cursor in place)
+    if (selection.rangeCount !== 0) {
+      // Store the original range
+      const range = window.getSelection().getRangeAt(0);
+      // Clone the range
+      const preCaretRange = range.cloneRange();
+      // Select all textual contents from the contenteditable element
+      preCaretRange.selectNodeContents(element);
+      // And set the range end to the original clicked position
+      preCaretRange.setEnd(range.endContainer, range.endOffset);
+      // Return the text length from contenteditable start to the range end
+      position = preCaretRange.toString().length;
+    }
+    return position;
+  }
 }
