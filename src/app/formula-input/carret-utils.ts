0fa3fd49396958915e9838c69d1748f2e4471e27
wandri <wandrille.guesdon@akur8-tech.com>
2020-12-29 17:07:34 +0100

    remove useless code

--- src/app/formula-input/carret-utils.ts
+++ src/app/formula-input/carret-utils.ts
@@ -17,26 +17,6 @@ export function getCaretIndex(element: Node): number {
   return position;
 }
 
-function getCaretCoordinates(): { x: number; y: number } {
-  let x = 0,
-    y = 0;
-  const selection = window.getSelection();
-  // Check if there is a selection (i.e. cursor in place)
-  if (selection.rangeCount !== 0) {
-    // Clone the range
-    const range = selection.getRangeAt(0).cloneRange();
-    // Collapse the range to the start, so there are not multiple chars selected
-    range.collapse(true);
-    // getCientRects returns all the positioning information we need
-    const rect = range.getClientRects()[0];
-    if (rect) {
-      x = rect.left; // since the caret is only 1px wide, left == right
-      y = rect.top; // top edge of the caret
-    }
-  }
-  return { x, y };
-}
-
 export function setCaret(index: number, element: HTMLElement): void {
   const range = document.createRange();
   const selection = window.getSelection();
